d21d74d8c0849022b5c0cce82287f04180e4a7a3
Grace Wan <grace_wan@brown.edu>
2023-02-16 20:44:48 -0500

    Fixing up tests

--- src/main.test.js
+++ src/main.test.js
@@ -1,17 +1,26 @@
 import * as main from "./main.js";
 import "@testing-library/jest-dom";
-var startHTML = "<div class=\"repl\">\n      <div class=\"repl-history\"></div>\n      <hr />\n      <div class=\"repl-input\">\n        <input type=\"text\" class=\"repl-command-box\" />\n        <button type=\"button\" class=\"submit-button\">submit</button>\n      </div>\n</div>";
-var maybeInput;
+import { screen } from "@testing-library/dom";
+import * as sd from "../mockData/SearchData.js";
+var startHTML = "<div class=\"repl\">\n<div class=\"repl-history\"></div>\n<hr />\n<div class=\"repl-input\">\n  <input\n    type=\"text\"\n    class=\"repl-command-box\"\n    placeholder=\"Enter command here!\"\n    id=\"input\"\n    ;\n  />\n  <button class=\"submit-button\">SUBMIT</button>\n</div>\n</div>";
+//let maybeInput: HTMLInputElement | null;
 beforeEach(function () {
     main.clearHistory();
     document.body.innerHTML = startHTML;
-    maybeInput = document.getElementById("repl-command-box");
+    // possibly don't need this
+    main.prepareButtonPress();
+    main.prepareEnterFeature();
+    main.prepareCSVList();
+    //maybeInput = document.getElementsByClassName("repl-command-box")[0];
 });
+// TODO: Make sure text is displayed on screen
 test("loading a file", function () {
-    if (maybeInput instanceof HTMLInputElement) {
-        maybeInput.value = "load_file csv1";
+    var commandBox = document.getElementsByClassName("repl-command-box")[0];
+    if (commandBox instanceof HTMLInputElement) {
+        commandBox.value = "load_file mockedData/csv1";
     }
     main.handleCommand();
+    //main.handleButtonPress(new MouseEvent("click"));
     var onfile = [
         ["1", "2", "3", "4", "5"],
         ["The", "song", "remains", "the", "same."],
@@ -19,40 +28,64 @@ test("loading a file", function () {
         ["C", "S", "3", "2", "."],
         ["WOO", "HOO", "BADABING", "BADA", "BOOM"],
     ];
-    var file = main.getloadedCSV()[0];
-    if (file != undefined) {
-        expect(file).toBe(onfile);
-    }
+    var file = main.getLoadedCSV();
+    expect(file).toEqual(onfile);
 });
 test("change mode", function () {
-    if (maybeInput instanceof HTMLInputElement) {
-        maybeInput.value = "mode";
+    var commandBox = document.getElementsByClassName("repl-command-box")[0];
+    if (commandBox instanceof HTMLInputElement) {
+        commandBox.value = "mode";
     }
     main.handleCommand();
-    var mod = main.getmode();
+    var mod = main.getMode();
     if (mod != undefined) {
-        expect(mod).toBe("Verbouse");
+        expect(mod).toEqual("VERBOSE");
     }
 });
 test("view", function () {
-    if (maybeInput instanceof HTMLInputElement) {
-        maybeInput.value = "load_file csv1";
+    var commandBox = document.getElementsByClassName("repl-command-box")[0];
+    if (commandBox instanceof HTMLInputElement) {
+        commandBox.value = "load_file mockedData/csv3";
     }
     main.handleCommand();
-    if (maybeInput instanceof HTMLInputElement) {
-        maybeInput.value = "view";
+    if (commandBox instanceof HTMLInputElement) {
+        commandBox.value = "view";
     }
     main.handleCommand();
-    expect(document.getElementById("repl-history")).toBe("");
+    var expectedValues = ["", "Aaron", "Barbara", "Clara", "Dylan"];
+    expectedValues.forEach(function (value) {
+        var expectedValue = screen.getByText(value);
+        expect(expectedValue).toBeDefined();
+    });
 });
 test("search", function () {
-    if (maybeInput instanceof HTMLInputElement) {
-        maybeInput.value = "load_file csv1";
+    var commandBox = document.getElementsByClassName("repl-command-box")[0];
+    if (commandBox instanceof HTMLInputElement) {
+        commandBox.value = "load_file mockedData/csv1";
     }
     main.handleCommand();
-    if (maybeInput instanceof HTMLInputElement) {
-        maybeInput.value = "search 0 3579";
+    if (commandBox instanceof HTMLInputElement) {
+        commandBox.value = "search 0 3579";
     }
     main.handleCommand();
-    expect(document.getElementById("repl-history")).toBe("");
+    expect(document.getElementsByClassName("repl-history")[0]).toContain([
+        "3759",
+        "96 G. Psc",
+        "7.26388",
+        "1.55643",
+        "0.68697",
+    ]);
+});
+// export {};
+test("emptyres", function () {
+    var s = new sd.SearchData();
+    expect(s.searchResult([[]], "0", "3759")).toEqual([
+        ["3759", "96 G. Psc", "7.26388", "1.55643", "0.68697"],
+    ]);
+    expect(s.searchResult([[]], "StarID", "3759")).toEqual([
+        ["3759", "96 G. Psc", "7.26388", "1.55643", "0.68697"],
+    ]);
+    expect(s.searchResult([[]], "2", "3759")).toEqual([
+        ["3759", "96 G. Psc", "7.26388", "1.55643", "0.68697"],
+    ]);
 });
--- src/main.test.ts
+++ src/main.test.ts
@@ -1,30 +1,46 @@
 import * as main from "./main.js";
 import "@testing-library/jest-dom";
+import { screen } from "@testing-library/dom";
+import * as sd from "../mockData/SearchData.js";
 
 const startHTML = `<div class="repl">
-      <div class="repl-history"></div>
-      <hr />
-      <div class="repl-input">
-        <input type="text" class="repl-command-box" />
-        <button type="button" class="submit-button">submit</button>
-      </div>
+<div class="repl-history"></div>
+<hr />
+<div class="repl-input">
+  <input
+    type="text"
+    class="repl-command-box"
+    placeholder="Enter command here!"
+    id="input"
+    ;
+  />
+  <button class="submit-button">SUBMIT</button>
+</div>
 </div>`;
 
-let maybeInput: HTMLElement | null;
+//let maybeInput: HTMLInputElement | null;
 
 beforeEach(() => {
   main.clearHistory();
   document.body.innerHTML = startHTML;
 
-  maybeInput = document.getElementById("repl-command-box");
+  // possibly don't need this
+  main.prepareButtonPress();
+  main.prepareEnterFeature();
+  main.prepareCSVList();
+
+  //maybeInput = document.getElementsByClassName("repl-command-box")[0];
 });
 
+// TODO: Make sure text is displayed on screen
 test("loading a file", () => {
-  if (maybeInput instanceof HTMLInputElement) {
-    maybeInput.value = "load_file csv1";
+  const commandBox = document.getElementsByClassName("repl-command-box")[0];
+  if (commandBox instanceof HTMLInputElement) {
+    commandBox.value = "load_file mockedData/csv1";
   }
 
   main.handleCommand();
+  //main.handleButtonPress(new MouseEvent("click"));
 
   const onfile: String[][] = [
     ["1", "2", "3", "4", "5"],
@@ -34,55 +50,81 @@ test("loading a file", () => {
     ["WOO", "HOO", "BADABING", "BADA", "BOOM"],
   ];
 
-  let file: String | undefined = main.getloadedCSV()[0];
-  if (file != undefined) {
-    expect(file).toBe(onfile);
-  }
+  let file: String[][] | undefined = main.getLoadedCSV();
+
+  expect(file).toEqual(onfile);
 });
 
 test("change mode", () => {
-  if (maybeInput instanceof HTMLInputElement) {
-    maybeInput.value = "mode";
+  const commandBox = document.getElementsByClassName("repl-command-box")[0];
+  if (commandBox instanceof HTMLInputElement) {
+    commandBox.value = "mode";
   }
 
   main.handleCommand();
 
-  let mod: String | undefined = main.getmode();
+  let mod: String | undefined = main.getMode();
   if (mod != undefined) {
-    expect(mod).toBe("Verbouse");
+    expect(mod).toEqual("VERBOSE");
   }
 });
 
 test("view", () => {
-  if (maybeInput instanceof HTMLInputElement) {
-    maybeInput.value = "load_file csv1";
+  const commandBox = document.getElementsByClassName("repl-command-box")[0];
+  if (commandBox instanceof HTMLInputElement) {
+    commandBox.value = "load_file mockedData/csv3";
   }
 
   main.handleCommand();
 
-  if (maybeInput instanceof HTMLInputElement) {
-    maybeInput.value = "view";
+  if (commandBox instanceof HTMLInputElement) {
+    commandBox.value = "view";
   }
 
   main.handleCommand();
 
-  expect(document.getElementById("repl-history")).toBe("");
+  const expectedValues = ["", "Aaron", "Barbara", "Clara", "Dylan"];
+
+  expectedValues.forEach((value) => {
+    const expectedValue = screen.getByText(value);
+    expect(expectedValue).toBeDefined();
+  });
 });
 
 test("search", () => {
-  if (maybeInput instanceof HTMLInputElement) {
-    maybeInput.value = "load_file csv1";
+  const commandBox = document.getElementsByClassName("repl-command-box")[0];
+  if (commandBox instanceof HTMLInputElement) {
+    commandBox.value = "load_file mockedData/csv1";
   }
 
   main.handleCommand();
 
-  if (maybeInput instanceof HTMLInputElement) {
-    maybeInput.value = "search 0 3579";
+  if (commandBox instanceof HTMLInputElement) {
+    commandBox.value = "search 0 3579";
   }
 
   main.handleCommand();
 
-  expect(document.getElementById("repl-history")).toBe("");
+  expect(document.getElementsByClassName("repl-history")[0]).toContain([
+    "3759",
+    "96 G. Psc",
+    "7.26388",
+    "1.55643",
+    "0.68697",
+  ]);
 });
 
-export {};
+// export {};
+
+test("emptyres", () => {
+  let s = new sd.SearchData();
+  expect(s.searchResult([[]], "0", "3759")).toEqual([
+    ["3759", "96 G. Psc", "7.26388", "1.55643", "0.68697"],
+  ]);
+  expect(s.searchResult([[]], "StarID", "3759")).toEqual([
+    ["3759", "96 G. Psc", "7.26388", "1.55643", "0.68697"],
+  ]);
+  expect(s.searchResult([[]], "2", "3759")).toEqual([
+    ["3759", "96 G. Psc", "7.26388", "1.55643", "0.68697"],
+  ]);
+});
